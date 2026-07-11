ceb5715bcdb9d3edf2b4ab63c58df315cf3c0788
Cole Gilbert <colegil2012@gmail.com>
2026-07-10 21:47:36 -0400

    Temporary debug port

--- launch.sh
+++ launch.sh
@@ -37,5 +37,5 @@ exec /usr/lib/chromium/chromium \
   --force-device-scale-factor=1 \
   --disable-session-crashed-bubble --disable-popup-blocking \
   --disable-gpu \
-  --use-gl=swiftshader \
+  --remote-debugging-port=9222 \
   --app=http://localhost:$PORT
